d7965e0dc7ff7052a97f5f486c0303e3c68e44b4
Paul Roe (r626961) <paul.roe@cambiahealth.com>
2015-11-06 13:35:57 -0800

    skeleton for initial API support, include updated server.js

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-    }).listen(port);
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// setup apps to have access to the bodyParser() features for parsing POST requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+// Base Routing 
+var baserouter = express.Router();
+baserouter.get('/', function(req,res) {
+   res.json({ message: 'HIP Services found here' });
+});
+
+
+// Register Routes
+app.use('/', baserouter);
+
+var clinic = require('./clinicAPI/routes/clinic');
+app.use('/clinic', clinic);
+
+
+var resolved_port = process.env.PORT || 3000;
+var server = app.listen( resolved_port, function () {
+   var host = server.address().address;
+   var port = server.address().port;
+
+   console.log('Example app listening at http://%s:%s', host, port);
+});
 
