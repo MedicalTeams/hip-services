ac2004c745dfcb36fb7da19ff4fac750c1eb227c
Paul Roe (r626961) <paul.roe@cambiahealth.com>
2015-11-18 09:10:50 -0800

    Rework of the services to use the swagger API model #2

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var app = express();
+'use strict';
 var bodyParser = require('body-parser');
+var app = require('connect')();
+var http = require('http');
+var swaggerTools = require('swagger-tools');
 var logger = require('./utils/logger');
 
 // setup apps to have access to the bodyParser() features for parsing POST requests
@@ -8,26 +10,35 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-// Base Routing
-var baserouter = express.Router();
-baserouter.get('/', function(req,res) {
-   res.json({ message: 'HIP Services found here' });
+var serverPort = process.env.PORT || 8080;
+
+// swaggerRouter configuration
+var options = {
+    swaggerUi: '/swagger.json',
+    controllers: './facilityAPI/controllers',
+    useStubs: process.env.NODE_ENV === 'development' ? true : false // Conditionally turn on stubs (mock mode)
+};
+
+// The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
+var swaggerDoc = require('./facilityAPI/swagger.json');
+
+// Initialize the Swagger middleware
+swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
+    // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
+    app.use(middleware.swaggerMetadata());
+
+    // Validate Swagger requests
+    app.use(middleware.swaggerValidator());
+
+    // Route validated requests to appropriate controller
+    app.use(middleware.swaggerRouter(options));
+
+    // Serve the Swagger documents and Swagger UI
+    app.use(middleware.swaggerUi());
+
+    // Start the server
+    http.createServer(app).listen(serverPort, function () {
+        console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
+        console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+    });
 });
-
-// Register Routes
-app.use('/', baserouter);
-
-var clinic = require('./clinicAPI/routes/clinic');
-clinic.connection = app.use('/clinic', clinic);
-
-
-var resolved_port = process.env.PORT || 3000;
-var server = app.listen( resolved_port, function () {
-   var host = server.address().address;
-   var port = server.address().port;
-
-
-
-   console.log('Example app listening at http://%s:%s', host, port);
-});
-
