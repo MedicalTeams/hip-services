6d6bfb4fed3d57c334f3e9f25b993473e0d883e1
Paul Roe (r626961) <paul.roe@cambiahealth.com>
2015-11-18 09:10:25 -0800

    Rework of the services to use the swagger API model

--- facilityAPI/dao/facilityDao.js
+++ facilityAPI/dao/facilityDao.js
@@ -1,44 +1,70 @@
-var express = require('express');
-var router = express.Router();
-var logger = require('../../utils/logger');
+var pool = require('./connectionpool.js');
 
-// middleware for logging
-router.use(function timeLog(req, res, next) {
-    //logger.info(req);
-    next();
-});
+//acquire a connection
+pool.acquire(function (err, connection) {
+    if (err)
+        console.error(err);
 
+    //use the connection as normal
+    var request = new Request('select 42', function(err, rowCount) {
+        if (err)
+            console.error(err);
 
-// Connection Stuff... move to it's own.
-var Connection = require('tedious').Connection;
-var config = {
-    userName: 'dbadmin',
-    password: 'password$1',
-    server: 'mti-dev-db.database.windows.net',
-    // If you are on Microsoft Azure, you need this:
-    options: {encrypt: true, database: 'Clinic'}
-};
-var connection = new Connection(config);
-connection.on('connect', function(err) {
-    // If no error, then good to proceed.
-    console.log("Connected");
-});
-var Request = require('tedious').Request
-var TYPES = require('tedious').TYPES;
+        console.log('rowCount: ' + rowCount);
 
-/*
- * GET list of clinic resources
- */
- router.get('/', function(req, res) {
-    res.json('["clinic1","clinic2","clinic3"]');
+        //release the connection back to the pool when finished
+        connection.release();
     });
 
+    request.on('row', function(columns) {
+        console.log('value: ' + columns[0].value);
+    });
+
+    connection.execSql(request);
+});
+
+pool.on('error', function(err) {
+    console.error(err);
+});
+
+
+
+
 /*
- * GET a clinic resources by clinic ID
+ * GET list of facilityRouter resources
+ */
+/*
+ * GET list of facilities
+ */
+router.get('/', function(req, res) {
+
+    request = new Request("SELECT faclty_id, hlth_care_faclty from faclty", function(err) {
+        if (err) {
+            console.log("the error: " + err);}
+        else {
+            console.log("fetched facilities")
+            connection.reset();
+            res.json(facilities);
+        }
+    });
+
+    var facilities = []
+    request.on('row', function(columns) {
+        console.log("found facility " + columns[1].value)
+        var facility = {};
+        facility[columns[0].value] = columns[1].value;
+        facilities.push(facility);
+        console.log(JSON.stringify(facilities));
+    });
+
+    connection.execSql(request);
+});
+/*
+ * GET a facilityRouter resources by facilityRouter ID
  */
 router.get('/:clinicId', function(req, res) {
     var cid = req.params.clinicId;
-    res.json({ cid: 'This is a clinic details object'});
+    res.json({ cid: 'This is a facilityRouter details object'});
 });
 
 router.get('/:clinicId/visit/:visitId', function(req, res) {
